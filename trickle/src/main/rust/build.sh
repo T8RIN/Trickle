58eefca7a2f9b634493acf25afd9ba2d0b68768c
Malik Mukhametzyanov <oolbp@bk.ru>
2026-07-06 01:39:29 +0300

    Added VVC decoder and encoder 1.8.6

--- trickle/src/main/rust/build.sh
+++ trickle/src/main/rust/build.sh
@@ -3,42 +3,26 @@ set -e
 
 cd "$(dirname "$0")"
 
-(
-  cd oxipng_jni
-  cargo ndk \
-    -t arm64-v8a \
-    -t armeabi-v7a \
-    -t x86_64-linux-android \
-    -o ../../libs \
-    build --release
-)
+export RUSTFLAGS="-C link-arg=-Wl,-z,max-page-size=16384"
 
-(
-  cd ntsc_jni
-  cargo ndk \
-    -t arm64-v8a \
-    -t armeabi-v7a \
-    -t x86_64-linux-android \
-    -o ../../libs \
-    build --release
-)
+build_crate() {
+  crate="$1"
 
-(
-  cd imagequant_jni
-  cargo ndk \
-    -t arm64-v8a \
-    -t armeabi-v7a \
-    -t x86_64-linux-android \
-    -o ../../libs \
-    build --release
-)
+  (
+    cd "$crate"
 
-(
-  cd vvc_jni
-  cargo ndk \
-    -t arm64-v8a \
-    -t armeabi-v7a \
-    -t x86_64-linux-android \
-    -o ../../libs \
-    build --release
-)
+    rustup run nightly cargo ndk \
+      -t arm64-v8a \
+      -t armeabi-v7a \
+      -t x86_64-linux-android \
+      -o ../../libs \
+      build \
+      -Z build-std=std,panic_abort \
+      --release
+  )
+}
+
+build_crate oxipng_jni
+build_crate ntsc_jni
+build_crate imagequant_jni
+build_crate vvc_jni
